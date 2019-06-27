271f798d96a278858cac36c90cc7d8d260717b65
Cesar Nicolas Cardozo Rincón <srfeudalwow@gmail.com>
2019-06-27 17:27:21 -0500

    fav

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import { PeliCardPage } from '../components/peli-card/peli-card.page';
+import { PeliCardPageModule } from '../components/peli-card/peli-card.module';
 
 @NgModule({
+  entryComponents:[
+    PeliCardPage
+  ],
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    PeliCardPageModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }])
   ],
   declarations: [Tab3Page]
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { IPeli } from '../IResultadoApi';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  pelis:IPeli[];
 
+  constructor(private storage: Storage) {}
+  
+  ngOnInit(){
+    this.pelis=[];
+    this.storage.length().then((keysLength: Number) => {
+      for(let i=0; i<keysLength;i++){
+        this.storage.get('peli-'+i).then((val:IPeli) => {
+          this.pelis[this.pelis.length]=val;
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+    });
+  }
+
+  updateFavs(){
+    this.pelis=[];
+    this.storage.length().then((keysLength: Number) => {
+      for(let i=0; i<keysLength;i++){
+        this.storage.get('new-'+i).then((val:IPeli) => {
+          this.pelis[this.pelis.length]=val;
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+    });
+  }
+
+  displayOrNoMessage(){
+    return this.pelis.length==0;
+  }
 }
